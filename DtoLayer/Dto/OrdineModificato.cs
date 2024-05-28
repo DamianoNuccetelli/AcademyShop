485f268a70ce059afa67922737f462871a8b5fec
DamianoNuccetelli <damianonuccetelli1997@gmail.com>
2024-05-28 10:25:38 +0200

    bug fix

---
+++ DtoLayer/Dto/OrdineModificato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoLayer.Dto
+{
+    public class OrdineModificatoDTO
+    {
+        public int Id { get; set; }
+        public int FkIdUtente { get; set; }
+        public int FkIdStato { get; set; }
+        public DateTime DataRegistrazione { get; set; }
+        public DateTime? DataAggiornamento { get; set; }
+       
+    }
+}
