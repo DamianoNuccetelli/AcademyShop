fb9a4fbb9bf9b6eb2f6d0782ce9cbe08717a556a
GabrieleA <gabrieleaggio26@gmail.com>
2024-05-31 15:41:18 +0200

    pulizia codice

--- AcademyShopAPI/Controllers/ProdottoController.cs
+++ AcademyShopAPI/Controllers/ProdottoController.cs
@@ -67,8 +67,7 @@ namespace AcademyShopAPI.Controllers
         {
 
             var prodotto = _mapper.Map<Prodotto>(prodottoDto);
-            prodotto.Id = id;
-            //prodotto.Quantità = _mapper.Map<int>(prodottoDto.Quantità); 
+            prodotto.Id = id; 
             var updateResult = await _prodottoBusiness.UpdateProdottoAsync(prodotto);
 
             return NoContent();
