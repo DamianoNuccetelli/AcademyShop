5936cbeb5b2c695cd2ec2638e862c46d32f964ee
Daniel1899 <danielroberti99@live.it>
2024-06-03 17:49:15 +0200

    Homepage

--- FrontEnd/academy/src/components/Header/Header.js
+++ FrontEnd/academy/src/components/Header/Header.js
@@ -2,22 +2,54 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
+import ProductCard from '../ProductCard/ProductCard';
 import './Header.css';
 
 
 const Header = () => {
+
+    const productsData = [
+        { id: 1, name: 'Product 1', price: 10 },
+        { id: 2, name: 'Product 2', price: 20 },
+        { id: 3, name: 'Product 3', price: 30 },
+    ];
+    
     return (
         <div className="header">
             <div className='title_container'>
                 <h1>Dashboard</h1>
                 <h2>Benvenuto, Mario</h2>
             </div>
-            <div className=''>
-            <div className='add_container'>
+            <div className='welcome_container'>
+                <div className='add_container'>
                     <h2>Add new product</h2>
                     <FontAwesomeIcon icon={faPlus} className="plus-icon" />
+                </div>
+                <div className='banner_container'>
+                    <div className='boxTemporaneo'></div>
+                </div>
             </div>
-        </div>
+            <div className='products_container'>
+                <h2>All product</h2>
+                <div className='products'>
+                {productsData.map(product => (
+            <ProductCard
+                key={product.id}
+                name={product.name}
+                price={product.price}
+            />
+            ))}
+                </div>
+                <div className='products'>
+                {productsData.map(product => (
+            <ProductCard
+                key={product.id}
+                name={product.name}
+                price={product.price}
+            />
+            ))}
+                </div>
+            </div>
         </div>
     );
 };
---
+++ FrontEnd/academy/src/components/ProductCard/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ProductCard.css';
+
+
+const ProductCard = () => {
+    return (
+        <div className='card_container'>
+            <p>afsgs</p>
+        </div>
+    );
+};
+
+export default ProductCard;
+
+
