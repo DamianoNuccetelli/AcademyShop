a122a5b5fa2d2bd611d79c3e55603b2826499125
Daniel1899 <danielroberti99@live.it>
2024-06-03 14:00:19 +0200

    Sistemata struttura progetto

---
+++ FrontEnd/academy/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Dashboard.css';
+import Navbar from '../Navbar/Navbar';
+import Header from '../Header/Header';
+
+
+const Dashboard = () => {
+    return (
+        <div className='dashboard'>
+            <Navbar/>
+            <Header />
+            <p>dzsff</p>
+        </div>
+    );
+};
+
+export default Dashboard;
+
