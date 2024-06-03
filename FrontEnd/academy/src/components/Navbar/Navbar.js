eb45d77e3aa24f5f8009484b45dfcf318fd11191
Daniel1899 <danielroberti99@live.it>
2024-06-03 16:06:06 +0200

    navbar

--- FrontEnd/academy/src/components/Navbar/Navbar.js
+++ FrontEnd/academy/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faShoppingCart , faBoxOpen  } from '@fortawesome/free-solid-svg-icons';
 
 import './Navbar.css';
 
@@ -10,17 +10,20 @@ const Navbar = () => {
         <div className="navbar">
             <div className="navbar_container">
                 <FontAwesomeIcon icon={faUserCircle} className="user-icon" />
-                <h3>Profilo</h3>
+                <h4>Profilo</h4>
            </div>
            <div className='center'>
                 <hr className="separator" />
            </div>
            <div className="navbar_container">
-                <FontAwesomeIcon icon={faUserCircle} className="user-icon" />
-                <h3>Prodotti</h3>
-                <FontAwesomeIcon icon={faUserCircle} className="user-icon" />
-                <h3>Ordini</h3>
-           </div>
+                <FontAwesomeIcon icon={faShoppingCart} className="user-icon mb-20" />
+                <h4 className='mb-20'>Prodotti</h4>
+                <FontAwesomeIcon icon={faBoxOpen } className="user-icon mb-20" />
+                <h4 className='mb-20'>Ordini</h4>
+            </div>
+            <div className="navbar_container">
+                
+            </div>
         </div>
     );
 };
