3f88b3efad7f89bba7d86ce3c4da67cfc61dff1c
Francesco Vinario <vinario.francesco@outlook.com>
2024-06-04 10:56:19 +0200

    Implementato routing login a prima apertura applicazione

--- FrontEnd/academy/src/App.js
+++ FrontEnd/academy/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
     return (
         <>
         <Routes>
+            <Route path="/" element={<Login />}/>
             <Route path="/Dashboard" element={<Dashboard />}/>
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
