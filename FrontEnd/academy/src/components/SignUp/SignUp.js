43bd4b2202c1498822de00468bef913623570efc
renatoflorea <renatoflorea13@gmail.com>
2024-06-04 14:24:10 +0200

    implementazione signup

--- FrontEnd/academy/src/components/SignUp/SignUp.js
+++ FrontEnd/academy/src/components/SignUp/SignUp.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.css';
-
 import logo from '../../img/Proconsul-Services.png';
 
 const SignUp = () => {
+    // Stato per ogni campo di input
+    const [nome, setNome] = useState('');
+    const [cognome, setCognome] = useState('');
+    const [cittaNascita, setCittaDiNascita] = useState('');
+    const [dataNascita, setAnnoNascita] = useState('');
+    const [provinciaNascita, setProvincia] = useState('');
+    const [sesso, setSesso] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [codiceFiscale, setCodiceFiscale] = useState('');
+
+    const UrlApiRoot='https://localhost:7031/users';
+
+  
+    // Funzione per gestire l'invio del form
+    const handleSubmit = async (event) => {
+        
+        event.preventDefault(); // Previene il comportamento di default del form
+        try {
+            const userData = {
+                nome, 
+                cognome,
+                cittaNascita,
+                dataNascita,
+                provinciaNascita,
+                sesso,
+                email,
+                password,
+                codiceFiscale
+ 
+            };
+            
+            console.log("Dati inviati:", userData); // Debugging
+
+            const response = await fetch(UrlApiRoot, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            });
+            console.log(response)
+
+            if (!response.ok) {
+                throw new Error('Errore di rete');
+            }
+
+            const data = await response.json();
+            console.log("Risposta del server:", data); // Debugging
+
+            // Altre operazioni con la risposta se necessario
+        } catch (error) {
+            console.error('Si è verificato un errore durante la registrazione:', error);
+        }
+    };
+
     return (
         <div className="form-container sign-up">
             <form>
@@ -12,29 +67,30 @@ const SignUp = () => {
 
                 <span>o usa la tua email per registrarti</span>
                 <div className="input-row">
-                    <input type="text" placeholder="Nome" />
-                    <input type="text" placeholder="Cognome" />
+                    <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+                    <input type="text" placeholder="Cognome" value={cognome} onChange={(e) => setCognome(e.target.value)} />
                 </div>
                 <div className="input-row">
-                    <input type="email" placeholder="Città di nascita" />
-                    <input type="text" placeholder="Provincia di nascita" />
+                    <input type="text" placeholder="Città di nascita" value={cittaNascita} onChange={(e) => setCittaDiNascita(e.target.value)} />
+                    <input type="text" placeholder="Provincia di nascita" value={provinciaNascita} onChange={(e) => setProvincia(e.target.value)} />
                 </div>
                 <div className="input-row">
-                    <input type="date" placeholder="Data di nascita" />
-                    <select name="cars" id="cars">
-                        <option value="choose">Seleziona il sesso</option>
+                    <input type="date" value={dataNascita} onChange={(e) => setAnnoNascita(e.target.value)} />
+                    <select value={sesso} onChange={(e) => setSesso(e.target.value)}>
+                        <option value="">Seleziona il sesso</option>
                         <option value="M">Maschio</option>
                         <option value="F">Femmina</option>
                     </select>
                 </div>
                 <div className="input-row">
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
+                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div className="input-single-row">
-                    <input type="text" placeholder="Codice fiscale" />
+                    <input type="text" placeholder="Codice fiscale" value={codiceFiscale} onChange={(e) => setCodiceFiscale(e.target.value)} />
                 </div>
-                <button>Registrati</button>
+                <button type="submit" onClick={handleSubmit}>Registrati</button>
+                
             </form>
         </div>
     );
